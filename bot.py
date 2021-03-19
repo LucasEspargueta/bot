899cdc263dd32b1a8e13c5a1532040aded398af5
PogfLux <80978330+PogfLux@users.noreply.github.com>
2021-03-19 17:53:22 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,8 @@ async def on_message(msg):
    if "pedro ramalho" in msg.content.lower():
       await msg.channel.send('Pedro Ramalho')
       await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/818850186754785310/unknown.png')
-   elif "dababy" in msg.content.lower():
+   elif "dababy" in msg.content.lower() or "less go" in msg.content.lower():
       await msg.channel.send('DaBaby')
-   elif "less go" in msg.content.lower():
       await msg.channel.send('https://cdn.discordapp.com/attachments/379034825304178712/822505085820862494/IMG_20210318_233715.jpg')
    elif "baIta" in msg.content:
       await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/822255453677289482/SPOILER_unknown.png')

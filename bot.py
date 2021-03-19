0e7213b90f3d5454b042e9bbe568675b24792281
LucasSexo <lukinha7529@gmail.com>
2021-03-19 16:55:30 +0000

    Updated bot!

---
+++ bot.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=['cum '])
+
+@client.event
+async def on_message(msg):
+   if msg.author == client.user:
+      return
+   if "pedro ramalho" in msg.content.lower():
+      await msg.channel.send('Pedro Ramalho')
+   elif   "dababy" in msg.content.lower():
+      await msg.channel.send('DaBaby')
+   elif   "less go" in msg.content.lower():
+      await msg.channel.send('https://cdn.discordapp.com/attachments/379034825304178712/822505085820862494/IMG_20210318_233715.jpg')
+   elif   "balta" in msg.content.lower():
+      await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/822255453677289482/SPOILER_unknown.png')
+   await client.process_commands(msg)
+
+@client.command()
+async def now(ctx):
+   await ctx.send('https://www.youtube.com/watch?v=uJ_1HMAGb4k')
+
+@client.command()
+async def gangnamstyle(ctx):
+   await ctx.send('https://www.youtube.com/watch?v=9bZkp7q19f0')
+
+@client.command()
+async def git(ctx):
+   await ctx.send('https://media.giphy.com/media/OOXp2e1gCnfj6jGxN9/giphy.gif')
+
+client.run('ODIyNTAyMTI3NjE5NjcwMDg2.YFTMzA.GQL5XXX2sF9dIhGi5suC4HxdubI')

89b3158fee5782c4facb2bf1abe2438706995b8f
LucasSexo <80959500+LucasSexo@users.noreply.github.com>
2021-03-24 15:17:55 +0000

    Update bot.py
    HORA DA ALTERNA

--- bot.py
+++ bot.py
@@ -1,47 +1,44 @@
-import os
-import re
-import random
-from discord.ext import commands
 import discord
-from pretty_help import PrettyHelp
+import youtube_dl
+from discord.ext import commands
+from discord.utils import get
+import os
+from datetime import datetime
+import time
+from pytz import timezone
 
-client = commands.Bot(command_prefix=['cum '], help_command=PrettyHelp())
-woo_regex = re.compile(r"woo+\b", flags=re.IGNORECASE)
+client = commands.Bot(command_prefix="cum ")
+
+global voice
+now = datetime.now(timezone('UTC'))
+
+t = time.localtime()
+global current_time
+
+async def toca():
+    ydl_opts = {'format': 'bestaudio'}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info('https://www.youtube.com/watch?v=1A_poY1VkKo', download=False)
+        URL = info['formats'][0]['url']
+    voice = get(client.voice_clients)
+    voice.play(discord.FFmpegPCMAudio(URL))
+
+async def hora():
+    time.sleep(1)
+    while True:
+        now = datetime.now(timezone('UTC'))
+        current_time = now.strftime("%H:%M:%S")
+        time.sleep(1)
+        if current_time == '23:15:00':
+            canalvoz = client.get_channel(655826315777146909)
+            canaltexto = client.get_channel(822774646981263360)
+            await canaltexto.send('HORA DA ALTERNA :clock11: ')
+            await canalvoz.connect()
+            time.sleep(3)
+            await toca()
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('Pedro Ramalho'))
-      
-@client.event
-async def on_message(msg):
-   if msg.author.bot:
-      return
-   if woo_regex.search(msg.content) is not None:
-      await msg.channel.send("https://tenor.com/view/pop-smoke-dance-nyc-dance-move-smile-gif-16391422")
-   if "among us" in msg.content.lower() or "amogus" in msg.content.lower():
-      await msg.channel.send('among us')
-   if "pedro ramalho" in msg.content.lower():
-      await msg.channel.send('Pedro Ramalho')
-      await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/818850186754785310/unknown.png')
-   if "dababy" in msg.content.lower() or "less go" in msg.content.lower():
-      await msg.channel.send("Let's go")
-      await msg.channel.send('https://cdn.discordapp.com/attachments/379034825304178712/822505085820862494/IMG_20210318_233715.jpg')
-   if "balta" in msg.content.lower():
-      num = random.randint(0, 1000000)
-      if num == 69420:
-        await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/822255453677289482/SPOILER_unknown.png')
-   await client.process_commands(msg)
-
-@client.command()
-async def now(ctx):
-   await ctx.send('https://www.youtube.com/watch?v=uJ_1HMAGb4k')
-
-@client.command()
-async def gangnamstyle(ctx):
-   await ctx.send('https://www.youtube.com/watch?v=9bZkp7q19f0')
-
-@client.command()
-async def git(ctx):
-   await ctx.send('https://media.giphy.com/media/OOXp2e1gCnfj6jGxN9/giphy.gif')
+    await hora()
 
 client.run(os.environ["token"])

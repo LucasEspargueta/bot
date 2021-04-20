9c0137e2024471eddfb8d86fb1f9fda1830a78aa
LucasSexo <80959500+LucasSexo@users.noreply.github.com>
2021-04-20 16:37:16 +0100

    No more alterna
    :( não funfava anyway

--- bot.py
+++ bot.py
@@ -1,61 +1,50 @@
-import discord
-import youtube_dl
-from discord.ext import commands
-from discord.utils import get
-import os
-from datetime import datetime
-import time
-from pytz import timezone
-import asyncio
 import random
+from discord.ext import commands
 from pretty_help import PrettyHelp
-import re
+import discord
 
 client = commands.Bot(command_prefix=['gimme '], help_command=PrettyHelp())
-woo_regex = re.compile(r"woo+\b", flags=re.IGNORECASE)
-global voice
-now = datetime.now(timezone('Europe/Lisbon'))
-t = time.localtime()
-global current_time
-
-async def toca():
-    ydl_opts = {'format': 'bestaudio'}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info('https://www.youtube.com/watch?v=1A_poY1VkKo', download=False)
-        URL = info['formats'][0]['url']
-    voice = get(client.voice_clients)
-    voice.play(discord.FFmpegPCMAudio(URL))
-
-async def hora():
-    await asyncio.sleep(1)
-    while True:
-        now = datetime.now(timezone('Europe/Lisbon'))
-        current_time = now.strftime("%H:%M:%S")
-        await asyncio.sleep(1)
-        if current_time == '23:15:00':
-            canalvoz = client.get_channel(759884692219625493)
-            canaltexto = client.get_channel(759882556744663040)
-            await asyncio.sleep(1)
-            await canaltexto.send(':clock11: HORA DA ALTERNA :clock11: \n' 'https://cdn.discordapp.com/attachments/759882556744663040/824573836468158484/booba.png')
-            await canalvoz.connect()
-            await asyncio.sleep(1)
-            await toca()
-            print("Hora da alterna começou")
-
 @client.event
 async def on_ready():
-    print('on')
-    await hora()
+    print('Vim-me!')
 
-@client.command(name="link", help="Generates a random Lightshot link")
+@client.command()
 async def link(ctx):
     lista = ["a", "b", "c", "d", "e", "f",
              "g", "h", "i", "j", "k", "l",
              "m", "n", "o", "p", "q", "r",
              "s", "t", "u", "v", "x", "y", "z",
              "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    print("Link criado")
-    await ctx.send("Caso contenha algo inadequado, por favor reporta abrindo o link.\n" "https://prnt.sc/" + "".join(list(random.choices(lista, k=6))))
+    await ctx.send("https://prnt.sc/" + "".join(list(random.choices(lista, k=6))))
 
-client.loop.create_task(hora())
+@client.event
+async def on_message(msg):
+   if msg.author == client.user:
+      return
+   if "woo" in msg.content.lower():
+      await msg.channel.send("https://tenor.com/view/pop-smoke-dance-nyc-dance-move-smile-gif-16391422")
+   if "among us" in msg.content.lower() or "amogus" in msg.content.lower():
+      await msg.channel.send('among us', tts=True)
+   if "pedro ramalho" in msg.content.lower():
+      await msg.channel.send('Pedro Ramalho', tts=True)
+      await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/818850186754785310/unknown.png')
+   elif "dababy" in msg.content.lower() or "less go" in msg.content.lower():
+      await msg.channel.send("Let's go - DaBaby", tts=True)
+      await msg.channel.send('https://cdn.discordapp.com/attachments/379034825304178712/822505085820862494/IMG_20210318_233715.jpg')
+   elif "balta" in msg.content.lower():
+      num = random.randint(0, 1000000)
+      if num == 69420:
+        await msg.channel.send('https://cdn.discordapp.com/attachments/759882556744663040/822255453677289482/SPOILER_unknown.png')
+        print('racismo')
+   await client.process_commands(msg)
+@client.command()
+async def cum(ctx):
+   await ctx.send('https://www.youtube.com/watch?v=uJ_1HMAGb4k')
+@client.command()
+async def gangnamstyle(ctx):
+   await ctx.send('https://www.youtube.com/watch?v=9bZkp7q19f0')
+@client.command()
+async def git(ctx):
+   await ctx.send('https://media.giphy.com/media/OOXp2e1gCnfj6jGxN9/giphy.gif')
+    
 client.run(os.environ["token"])

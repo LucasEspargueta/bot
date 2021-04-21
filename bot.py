4e8e9db204d4274f413dbcb4715577141c6c8f44
Luís Osório <80978330+PogfLux@users.noreply.github.com>
2021-04-21 09:55:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def link(ctx):
 
 @client.event
 async def on_message(msg):
-   if msg.author.id = 74434231882747904:
+   if msg.author.id = 192235561371697152:
       await msg.channel.send("Tony")
    if msg.author == client.user:
       return

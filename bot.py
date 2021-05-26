8806581cffdb1759ab3cb7f7768957dc2e571a9c
LucasSexo <lukinha7529@gmail.com>
2021-05-27 00:22:57 +0100

    fixed duped sending

--- bot.py
+++ bot.py
@@ -147,9 +147,9 @@ async def time(ctx, command="TUGA"):
 async def on_message(msg):
     if msg.author == client.user:
        return
-    for k, e in EMOTES.items():
-        if k in msg.content:
-            await msg.channel.send(e)
+    for s in msg.content.split():
+        if s in EMOTES:
+            await msg.channel.send(EMOTES[s])
     if msg.author == msg.guild.get_member(331231536106176512):
         mention = f'<@!{client.user.id}>'
         if "amo-te" in msg.content.lower() and mention in msg.content.lower(): 

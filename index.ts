53c41e16ed84b69e3bc79fd9bce94ff651c61235
LucasSexo <lukinha7529@gmail.com>
2021-11-24 01:10:51 +0000

    ACABA MARI

--- index.ts
+++ index.ts
@@ -7,11 +7,23 @@ const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
     ]
 })
 
-client.on('ready', () =>{
+client.on('ready', async () => {
     console.log("Tou ligado bro siga siga!")
     const guildID = '759849368966004767'
     const guild = client.guilds.cache.get(guildID)
@@ -21,10 +33,6 @@ client.on('ready', () =>{
     } else {
         commands = client.application?.commands
     }
-    commands?.create({
-        name: "ping",
-        description: "responde pong ajaj xd. ri-te :gun:",
-    })
     commands?.create({
         name: "screenshot",
         description: "Shows you a random screenshot from the web, if you see anything inappropriate please report it.",
@@ -39,16 +47,41 @@ client.on('ready', () =>{
                 type: ApplicationCommandOptionType.String,
                 required: true,
             },
-        ] 
-    })
+        ]
+    });
+    (await commands?.create({
+        name: "comm",
+        description: "Admin only command",
+        options: [
+            {
+                name: "role",
+                description: "The role you want to send the message to",
+                type: ApplicationCommandOptionType.Role,
+                required: true,
+            },
+            {
+                name: "message",
+                description: "The message you want to send",
+                type: ApplicationCommandOptionType.String,
+                required: true,
+            },
+        ],
+        defaultPermission: false
+
+    }))?.permissions.add({
+        guild: guildID,
+        permissions: [{
+            id: "759882119799111681", type: "ROLE", permission: true,
+        }]
+    });
 })
 
 function screenshotfn() {
-    const lista=["a", "b", "c", "d", "e", "f",
-    "g", "h", "i", "j", "k", "l",
-    "m", "n", "o", "p", "q", "r",
-    "s", "t", "u", "v", "x", "y", "z",
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    const lista = ["a", "b", "c", "d", "e", "f",
+        "g", "h", "i", "j", "k", "l",
+        "m", "n", "o", "p", "q", "r",
+        "s", "t", "u", "v", "x", "y", "z",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     return lista.sort(() => Math.random() - Math.random()).slice(0, 6).join("")
 }
 //random color generator
@@ -61,16 +94,16 @@ const randomColor = () => {
     return color
 }
 
-const ventfn=async(interaction: CommandInteraction)=>{
+const ventfn = async (interaction: CommandInteraction) => {
     const desabafo = await client.channels.fetch("796509327997403156")
-    const embed= new DiscordJS.MessageEmbed({
+    const embed = new DiscordJS.MessageEmbed({
         title: "Anon says",
-        description: interaction.options.getString("message",true),
+        description: interaction.options.getString("message", true),
         color: parseInt(randomColor().replace("#", "0x"), 16),
         timestamp: new Date(),
     })
     if (desabafo?.isText()) {
-        desabafo.send({embeds: [embed]})
+        desabafo.send({ embeds: [embed] })
     }
     interaction.reply({
         ephemeral: true,
@@ -78,17 +111,32 @@ const ventfn=async(interaction: CommandInteraction)=>{
     })
 }
 
-client.on('interactionCreate', async (interaction) =>{
+const commfn = async (interaction: CommandInteraction) => {
+    const role = interaction.options.getRole("role", true)
+
+    if (role instanceof DiscordJS.Role) {
+        role.members.forEach(member => {
+            member.send(interaction.options.getString("message", true))
+        });
+        interaction.reply({
+            ephemeral: true,
+            content: "Message sent!"
+        })
+        return
+
+    }
+    interaction.reply({
+        ephemeral: true,
+        content: "Message not sent!"
+    })
+}
+
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) {
         return
     }
-    const { commandName} = interaction
+    const { commandName } = interaction
     switch (commandName) {
-        case "ping":
-            interaction.reply({
-                content: "pong",
-            })
-            break;
         case "screenshot":
             interaction.reply({
                 content: ('https://prnt.sc/' + screenshotfn()),
@@ -97,9 +145,20 @@ client.on('interactionCreate', async (interaction) =>{
         case "vent":
             ventfn(interaction)
             break;
+        case "comm":
+            commfn(interaction)
+            break;
         default:
             break;
     }
 })
 
-client.login(process.env.MAMAS)
+client.login(process.env.MAMAS).then(() => {
+    client.user?.setPresence({
+        activities: [{
+            name: 'FEUPers behaviour',
+            type: 'WATCHING'
+        }],
+        status: 'idle'
+    });
+});

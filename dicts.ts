1fbf2ab7c95f81d8d3b216bd7cc788c68406809d
LucasSexo <80959500+LucasSexo@users.noreply.github.com>
2022-04-01 10:47:18 +0000

    huh
    https://cdn.frankerfacez.com/emoticon/660211/4

--- dicts.ts
+++ dicts.ts
@@ -80,4 +80,5 @@ export const EMOTES: Record<string,string> = {
     "Prayge": "https://cdn.frankerfacez.com/emoticon/625520/4",
     "vacuna": "https://cdn.discordapp.com/attachments/913610098012717116/932400701999546398/FHYwlB6XEAISP7P.png",
     "Despair": "https://i.kym-cdn.com/photos/images/newsfeed/001/996/641/bc2.jpg",
+    "HUH": "https://cdn.frankerfacez.com/emoticon/660211/4",
 }
